e56b37d8a74c8b88f837da50885570cb02079aa3
ChengshengDeng <chengshengdeng0308@163.com>
2023-05-29 16:21:26 +0800

    14

---
+++ 算法/14.最长公共前缀/14.solution.py
@@ -0,0 +1,16 @@
+"""
+比较最大值和最小值 
+"""
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str: 
+        if not strs: return ""
+        s1 = min(strs)
+        s2 = max(strs)
+        for i,x in enumerate(s1):
+            if x != s2[i]:
+                return s1[:i]
+        return s1 
+    
+
+s = Solution()
+s.longestCommonPrefix(['flower', 'flight','pig'])
